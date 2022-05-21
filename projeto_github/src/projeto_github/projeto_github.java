05e296326ed730237c2f91df16b1573fe6079f6e
deciodani <62177400+deciodani@users.noreply.github.com>
2022-05-21 12:23:40 -0300

    Add files via upload

---
+++ projeto_github/src/projeto_github/projeto_github.java
@@ -0,0 +1,11 @@
+package projeto_github;
+
+/** Aplicação Hello World */
+public class projeto_github {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Alo , mundo!");
+		
+	}
+}
